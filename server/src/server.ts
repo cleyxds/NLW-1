18c6ce487e643159925604df7204a9398e0b8b91
cleyxds <cleysonn@hotmail.com.br>
2020-06-03 22:18:05 -0300

    back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
+import routes from './routes';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (req,res) => {
-  console.log('Listagem de usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  res.json([
-    'cleyxds'
-  ]);
-
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
